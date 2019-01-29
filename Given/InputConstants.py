835eeb108a67b2d3741a775e007580fead7b31ed
Ali <zamaniali1995@gmail.com>
2019-01-29 22:30:14 +0330

    add tensorflow

--- Given/InputConstants.py
+++ Given/InputConstants.py
@@ -15,7 +15,15 @@ class Inputs:
 #    END_OF_NODES_DELIMETER = "***END OF NODE***"
 #    START_OF_LINK_DELIMETER = "***START OF LINK***"
 #    END_OF_LINK_DELIMETER = "***END OF LINK***"
+# network topology parameters
     network_topology_node_name = 0
     network_topology_node_cap = 1
     network_topology_link_dis = 1
     network_topology_link_cap = 2
+# Learning parameters
+    epochNum = 5000
+    batchSize = 500
+    trainNum=3500
+    validNum=100
+    node_features = 4
+    learningRate = 1e-6
--- Main/main.py
+++ Main/main.py
@@ -10,22 +10,12 @@ sys.path.insert(0, '../ReadFile')
 sys.path.insert(0, '../Given')
 import InputConstants
 from ReadFile import Graph
-#class node:
-#    def __init__(self, name, cap, degree):
-#        self.name = name
-#        self.cap = cap
-#        self.degree = degree
-#        self.function = 'None'
-#    def placement(self, function):
-#        self.function = function
-##class ServiceChain:
-##    def __init__(self, )
-## Main
-#a = 1
-#v1 = node('node_1', 20, 3)
-#v2 = node('node_2', 20, 4)
+import tensorflow
 input_cons = InputConstants.Inputs()
 graph = Graph(input_cons.network_path + input_cons.network_name)
+
+# Learning
+
 #print(graph.start_file_line)
 
 #print (IndentationError.read_path)
