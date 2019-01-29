a19cb6068c5087c1fd917d1255b23a5f82598c14
Ali <zamaniali1995@gmail.com>
2019-01-29 17:10:24 +0330

    modified input file

--- Given/InputConstants.py
+++ Given/InputConstants.py
@@ -8,10 +8,12 @@ Created on Sun Jan 27 17:58:40 2019
 
 class Inputs:
     network_path = "../Data/"
-    network_name = "nsf_14_network.txt"
-    START_OF_FILE_DELIMETER = '***START OF FILE***'
-    END_OF_FILE_DELIMETER = '***END OF FILE***'
-    START_OF_NODES_DELIMETER = "***START OF NODE***"
-    END_OF_NODES_DELIMETER = "***END OF NODE***"
-    START_OF_LINK_DELIMETER = "***START OF LINK***"
-    END_OF_LINK_DELIMETER = "***END OF LINK***"
+    network_name = "nsf_14_network.json"
+#    START_OF_FILE_DELIMETER = '***START OF FILE***'
+#    END_OF_FILE_DELIMETER = '***END OF FILE***'
+#    START_OF_NODES_DELIMETER = "***START OF NODE***"
+#    END_OF_NODES_DELIMETER = "***END OF NODE***"
+#    START_OF_LINK_DELIMETER = "***START OF LINK***"
+#    END_OF_LINK_DELIMETER = "***END OF LINK***"
+    network_topology_node_name = 0
+    network_topology_node_cap = 1
--- ReadFile/ReadFile.py
+++ ReadFile/ReadFile.py
@@ -10,7 +10,8 @@ Created on Sun Jan 27 19:24:24 2019
 import sys
 sys.path.insert(0, '../ReadFile')
 import InputConstants
-
+import csv
+import json
 ## Node features class
 class node:
     def __init__(self, name, cap, deg, bandwidth, dis):
@@ -36,53 +37,69 @@ class Graph:
         end_node_line = None
         start_link_line = None
         end_link_line = None
-        node_list = []
+        node_cap_list = []
         link_list = []
         
-        self.node_list = []
+#        self.node_list = []
         self.link_list = []
         
         input_cons = InputConstants.Inputs()
-        with open(path, 'r') as data:
-            for cnt, line in enumerate(data):
-                line = line.split(',')
-                if line[0] == input_cons.START_OF_FILE_DELIMETER:
-                    start_file_line = cnt
-                elif line[0] == input_cons.END_OF_FILE_DELIMETER:
-                    end_file_line = cnt
-                elif line[0] == input_cons.START_OF_NODES_DELIMETER:
-                    start_node_line = cnt
-                elif line[0] == input_cons.END_OF_NODES_DELIMETER:
-                    end_node_line = cnt
-                elif line[0] == input_cons.START_OF_LINK_DELIMETER:
-                    start_link_line = cnt
-                elif line[0] == input_cons.END_OF_LINK_DELIMETER:
-                    end_link_line = cnt
-        if start_file_line == None:
-            print('ReadFile Error: missing "START OF FILE DELIMETER"' )
-        elif end_file_line == None:
-            print('ReadFile Error: missing "END OF FILE DELIMETER"' )
-        elif start_node_line == None:
-            print('ReadFile Error: missing "START OF NODE DELIMETER"' )
-        elif end_node_line == None:
-            print('ReadFile Error: missing "END OF NODE DELIMETER"' )
-        elif start_link_line == None:
-            print('ReadFile Error: missing "START OF LINK DELIMETER"' )
-        elif end_link_line == None:
-            print('ReadFile Error: missing "END OF LINK DELIMETER"' ) 
-        with open(path, 'r') as data:
-            for cnt, line in enumerate(data):
-                if start_node_line < cnt < end_node_line:
-                    node_list.append(line[:])
-                if start_link_line < cnt < end_link_line:
-                    link_list.append(line[:])
-        for i in range(len(node_list)):
-            self.node_list.append(node(node_list[i], 2, 4, 4, 10))
-        
-        for i in range(len(link_list)):
-            self.link_list.append(link(link_list[i], 2, 2, 2))
-            
-        self.v1 = node('1', 2, 4, 4, 10)
+#        data = pd.read_csv(path, header=None)
+#        print(data)
+        with open(path, "r") as data_file:
+            data = json.load(data_file)
+            tmp = data['networkTopology']['nodes']
+            node_name_list = [data['networkTopology']['nodes']
+                [i][input_cons.network_topology_node_name] 
+                    for i in range(len(tmp))]
+#            node_cap_list = [data['networkTopology']['nodes'][i][input_cons.network_topology_node_cap] for i in range(len(tmp))]
+#            link_list = [data['networkTopology']['links'][i] for i in self.node_name_list]
+            self.node_list = [node(node_name_list[cnt], data['networkTopology']
+                ['nodes'][cnt][input_cons.network_topology_node_cap],
+                    len(data['networkTopology']['links'][name]), 10, 10) 
+                        for cnt, name in enumerate(node_name_list)]
+#            print(len(tmp))
+#        with open(path, 'r') as data:
+#            for cnt, line in enumerate(data):
+#                line = line.split(',')
+#                if line[0] == input_cons.START_OF_FILE_DELIMETER:
+#                    start_file_line = cnt
+#                elif line[0] == input_cons.END_OF_FILE_DELIMETER:
+#                    end_file_line = cnt
+#                elif line[0] == input_cons.START_OF_NODES_DELIMETER:
+#                    start_node_line = cnt
+#                elif line[0] == input_cons.END_OF_NODES_DELIMETER:
+#                    end_node_line = cnt
+#                elif line[0] == input_cons.START_OF_LINK_DELIMETER:
+#                    start_link_line = cnt
+#                elif line[0] == input_cons.END_OF_LINK_DELIMETER:
+#                    end_link_line = cnt
+#        if start_file_line == None:
+#            print('ReadFile Error: missing "START OF FILE DELIMETER"' )
+#        elif end_file_line == None:
+#            print('ReadFile Error: missing "END OF FILE DELIMETER"' )
+#        elif start_node_line == None:
+#            print('ReadFile Error: missing "START OF NODE DELIMETER"' )
+#        elif end_node_line == None:
+#            print('ReadFile Error: missing "END OF NODE DELIMETER"' )
+#        elif start_link_line == None:
+#            print('ReadFile Error: missing "START OF LINK DELIMETER"' )
+#        elif end_link_line == None:
+#            print('ReadFile Error: missing "END OF LINK DELIMETER"' ) 
+#        with open(path, 'r') as data:
+#            for cnt, line in enumerate(data):
+#                if start_node_line < cnt < end_node_line:
+#                    tmp = csv.reader(line, delimiter=',')
+#                    print(tmp[0])
+#                    node_list.append(tmp)
+#                if start_link_line < cnt < end_link_line:
+#                    link_list.append(line[:])
+#        for i in range(len(node_list)):
+#            self.node_list.append(node(node_list[i], 2, 4, 4, 10))
+#        
+#        for i in range(len(link_list)):
+#            self.link_list.append(link(link_list[i], 2, 2, 2))
+#            
 
 #    def makegragh(self, node):
 #        node('v1' ,2, 5, 10, 50)
